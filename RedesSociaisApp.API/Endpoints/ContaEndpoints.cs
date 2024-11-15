c762d07ba2afbf6b308a939c313036a21e1ef506
pamelakelly0023 <pamela.kelly95@gmail.com>
2024-11-15 15:20:51 -0300

    Remove endpoint GetbyId

--- RedesSociaisApp.API/Endpoints/ContaEndpoints.cs
+++ RedesSociaisApp.API/Endpoints/ContaEndpoints.cs
@@ -9,11 +9,6 @@ namespace RedesSociaisApp.API.Endpoints
         public static void MapContaEndpoint(this IEndpointRouteBuilder app)
         {
             var conta = app.MapGroup("/api/v1/conta");
-
-            conta.MapGet("detalhes/{id}", static async (IMediator mediator, int id)
-                => await mediator.Send(new ObterContaRequest(id)))
-                    .WithName("Detalhes Conta");
-                    // .RequireAuthorization();
             
             conta.MapPost("criar/", static async (IMediator mediator, CriarContaRequest request)
                 => await mediator.Send(request))
@@ -21,17 +16,18 @@ namespace RedesSociaisApp.API.Endpoints
 
             conta.MapPut("alterar/{id}", static async (IMediator mediator, int id, AlterarContaRequest request) 
                 => await mediator.Send(request))
-                    .WithName("Alterar Conta");
+                    .WithName("Alterar Conta")
+                    .RequireAuthorization();
 
             conta.MapDelete("remover/{id}", static async (IMediator mediator, int id)
                 => await mediator.Send(new RemoverContaRequest(id)))
-                    .WithName("Exluir Conta");
-                    // .RequireAuthorization();
+                    .WithName("Exluir Conta")
+                    .RequireAuthorization();
 
             conta.MapPut("alterar-senha/{id}", static async (IMediator mediator, int id, AlterarSenhaContaRequest request) 
                 => await mediator.Send(request))
-                .WithName("Alterar senha Conta");
-                // .RequireAuthorization();
+                .WithName("Alterar senha Conta")
+                .RequireAuthorization();
             
             conta.MapPut("login", static async (IMediator mediator, LoginContaRequest request) 
                 => await mediator.Send(request))
