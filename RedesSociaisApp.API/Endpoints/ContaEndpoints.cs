58a41b66e332a747cd4cb5b9d6d19f7751764a37
pamelakelly0023 <pamela.kelly95@gmail.com>
2025-05-12 13:51:09 -0300

    Corrige endpoints

--- RedesSociaisApp.API/Endpoints/ContaEndpoints.cs
+++ RedesSociaisApp.API/Endpoints/ContaEndpoints.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using OperationResult;
 using RedesSociaisApp.API.Extensions;
 using RedesSociaisApp.Application.Exceptions;
@@ -17,33 +18,33 @@ namespace RedesSociaisApp.API.Endpoints
             var conta = app.MapGroup("/api/v1/conta")
                 .WithTags("Conta");
             
-            conta.MapPost("/", async (IMediator mediator, CriarContaRequest request) 
+            conta.MapPost("/", async (IMediator mediator, [FromBody]CriarContaRequest request) 
                 => await mediator.SendCommand(request))
                     .WithDisplayName("Endpoint para Cadastrar Conta")
                     .WithName("CadastrarConta")
                     .Produces<CriarContaResponse>(201);
 
-            conta.MapPut("/{id}", async (IMediator mediator, int id, AlterarContaRequest request) 
+            conta.MapPut("/{id}", async (IMediator mediator, int id, [FromBody]AlterarContaRequest request) 
                 =>  await mediator.Send(request))
                     .WithDisplayName("Endpoint para Alterar dados de Conta do Usuário")
                     .WithName("Alterar Conta")
                     .Produces<Result>(204)
                     .RequireAuthorization();
 
-            conta.MapDelete("/{id}", static async (IMediator mediator, RemoverContaRequest request)
-                =>  await mediator.SendCommand(request))
+            conta.MapDelete("/{id}", static async (IMediator mediator, [FromBody]RemoverContaRequest request)
+                =>  await mediator.Send(request))
                     .WithDisplayName("Endpoint para Excluir Conta do Usuário")
                     .WithName("Exluir Conta")
                     .Produces<Result>(204)
                     .Produces<Result>(404)
                     .RequireAuthorization();
 
-            conta.MapPut("alterar-senha/{id}", static async (IMediator mediator, int id, AlterarSenhaContaRequest request) 
+            conta.MapPut("alterar-senha/{id}", static async (IMediator mediator, int id, [FromBody]AlterarSenhaContaRequest request) 
                 => await mediator.Send(request))
                     .WithName("Alterar senha Conta");
                     // .RequireAuthorization();
             
-            conta.MapPut("login", static async (IMediator mediator, LoginContaRequest request) 
+            conta.MapPut("login", static async (IMediator mediator, [FromBody]LoginContaRequest request) 
                 => await mediator.Send(request) switch
                 {
                     (true, _) => Results.StatusCode(200),
